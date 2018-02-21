f8a90db93a4dfc24400849b3eb037573f4e70310
saradaniellesmith <33591569+saradaniellesmith@users.noreply.github.com>
2018-02-21 09:50:04 -0600

    Update closures.js
    problem 8 updated

--- closures.js
+++ closures.js
@@ -200,7 +200,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
